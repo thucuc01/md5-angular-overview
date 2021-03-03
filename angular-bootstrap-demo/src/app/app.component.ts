bb622c7ea494c05021aace02bf270905be96ca55
thucuc01 <70868459+thucuc01@users.noreply.github.com>
2021-03-03 22:29:21 +0700

    binding

---
+++ angular-bootstrap-demo/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+
+interface ChangeSizeParams {
+  value: any;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'Demo';
+  name = "";
+  fontSize=14;
+  ngOnInit(): void {
+  }
+  clearValue(){
+    this.name='';
+  };
+
+  // changeSize({value}: ChangeSizeParams){
+  //   this.fontSize=value;
+  // }
+
+
+
+
+}
